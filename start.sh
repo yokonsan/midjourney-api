fe3e0830176549b0e7a68ba63bb4a04580320ca8
yokon <15152347277@163.com>
2023-05-31 10:12:40 +0800

    chore

--- start.sh
+++ start.sh
@@ -1,18 +1,24 @@
 docker rm -f mj-server mj-bot
 #docker rmi kunyu/midjourney-api:1.0
 
-docker run -d --name mj-server -p 8062:8062 \
+docker network create mjapi
+docker run -d --net mjapi --name mj-server -p 8062:8062 \
 	-e TZ=Asia/Shanghai \
+	-e LOG_LEVEL=DEBUG \
 	-e USER_TOKEN="" \
 	-e GUILD_ID="" \
 	-e CHANNEL_ID="" \
+	-e CONCUR_SIZE=3 \
+	-e WAIT_SIZE=10 \
 	kunyu/midjourney-api:1.0
 
-docker run -d --name mj-bot \
+docker run -d --net mjapi --name mj-bot \
 	-e TZ=Asia/Shanghai \
+	-e LOG_LEVEL=DEBUG \
 	-e USER_TOKEN="" \
 	-e BOT_TOKEN="" \
 	-e GUILD_ID="" \
 	-e CHANNEL_ID="" \
 	-e CALLBACK_URL="" \
+	-e QUEUE_RELEASE_API="http://mj-server:8062/v1/api/trigger/queue/release" \
 	kunyu/midjourney-api:1.0 bot
